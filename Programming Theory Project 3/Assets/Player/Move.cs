aa9a9719210ea259da4456ffe6b637cf79dcd133
DaoQuocViet18 <108569142+DaoQuocViet18@users.noreply.github.com>
2022-07-09 21:45:26 +0700

    move + follow

--- Programming Theory Project 3/Assets/Player/Move.cs
+++ Programming Theory Project 3/Assets/Player/Move.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    private float speed = 10.0f;
+    private float turnSpeed = 25.0f;
+    public float HorizontalInput;
+    public float forwardInput;
+
+    private Rigidbody PlayerRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        move();
+    }
+
+    void move()
+    {
+        HorizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+
+        PlayerRb.AddForce(Vector3.forward * speed * forwardInput);                                                         
+        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * HorizontalInput);
     }
 }
