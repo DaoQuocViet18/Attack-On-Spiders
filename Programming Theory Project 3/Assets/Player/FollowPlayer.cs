091826e3bd3feeb45afe3d96a8d34525925d2847
DaoQuocViet18 <108569142+DaoQuocViet18@users.noreply.github.com>
2022-07-10 09:25:24 +0700

    move + cam

--- Programming Theory Project 3/Assets/Player/FollowPlayer.cs
+++ Programming Theory Project 3/Assets/Player/FollowPlayer.cs
@@ -4,19 +4,11 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+    public GameObject camPosition;
 
-    public GameObject player1;
-
-    private Vector3 offset = new Vector3(0, 0.8f, 0.3f);            // tr? ?i chi?u cao - chi?u dài c?a v?t th? --> vì trí camera mong mu?n
-
-    void Start()
+    // Update is called once per frame
+    void Update()
     {
-
-    }
-
-    void LateUpdate()
-    {
-        // offset the camera behind the player by adding to the player's position
-        transform.position = player1.transform.position + offset;
+        transform.position = camPosition.transform.position;
     }
 }
--- Programming Theory Project 3/Assets/Player/Move.cs
+++ Programming Theory Project 3/Assets/Player/Move.cs
@@ -4,33 +4,42 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    private float speed = 20.0f;
-    private float turnSpeed = 25.0f;
+    [Header("Movement")]
+    private float speed = 7.0f;
     public float HorizontalInput;
     public float forwardInput;
-
+    public Transform orientation;
+    Vector3 moveDirection;
     private Rigidbody PlayerRb;
 
     // Start is called before the first frame update
     void Start()
     {
         PlayerRb = GetComponent<Rigidbody>();
+        PlayerRb.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        move();
+        MoveInput();
     }
 
-    void move()
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MoveInput()
     {
         HorizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
+    }
 
+    void MovePlayer()
+    {
+        moveDirection = (orientation.transform.forward * forwardInput + orientation.right * HorizontalInput).normalized;
 
-     //   PlayerRb.AddForce(Vector3.forward * speed * forwardInput);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * HorizontalInput);
+        PlayerRb.AddForce(moveDirection * speed * 3.0f, ForceMode.Force);
     }
 }
